951eefd35b242f9a1d52a40da23ed88b9fc1bf97
kaue gatto <kauegatto123@gmail.com>
2021-10-20 17:35:48 -0300

    rota de store do token tá funcional

--- backend/src/controllers/TokenController.js
+++ backend/src/controllers/TokenController.js
@@ -11,21 +11,22 @@ class TokenController {
     const { email, password } = req.body;
     if (!(email && password)) {
       res.status(400);
-      res.json({ errors: ['invalid login data, you should pass email and password'] });
+      res.json({ errors: ['Dados de login inválidos, você deve preencher ambos os campos'] });
     }
     try {
       /* ver se usuario existe*/
       const userExists = await userModel.userExists(email);
       if (!userExists) {
         res.status(400);
-        res.json({ errors: ['User does not exist'] });
+        res.json({ errors: ['Usuário não existe'] });
         return;
       }
       /*ver se a senha é válida*/
-      /*
-        res.status(401).json({ errors: ['invalid password'] });
+      const validPass = await userModel.validateLogin(email, password);
+      if (!validPass) {
+        res.status(401).json({ errors: ['Senha inválida'] });
         return;
-      */
+      }
       const token = jwt.sign(
         { email }, // payload
         process.env.TOKEN_SECRET, // secret
--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -1,18 +1,41 @@
 import { connection } from '../db/dbConnection';
 class UserModel {
   async userExists(email) {
-    const query = 'SELECT * FROM `paciente` WHERE `nm_login_paciente` = ?';
-    let queryErrors, queryResults;
-    let valid = 2;
+    const query = 'SELECT * FROM paciente WHERE nm_login_paciente = ?;';
     /* aqui usamos um prepared statement, com o execute, logo também não ficamos vulneráveis à sql injection!
     http://stackoverflow.com/questions/8263371/how-can-prepared-statements-protect-from-sql-injection-attacks
     */
-    const [rows, fields, err] = await connection.execute(query, [email]);
-    if (rows && !err) {
-      console.log('opa');
-      return true;
+    try {
+      const [rows, fields, err] = await connection.execute(query, [email]);
+      if (rows[0] && !err) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
-    else {
+    catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+  async validateLogin(email, password) {
+    const query = `CALL LOGINPACIENTE('${email}','${password}');`;
+    let valid = 2;
+    try {
+      const [rows, fields, err] = await connection.execute(query);
+      /* rows tem 2 array: 1-> array dos resultados, json dentro, 2-> array de info do bd*/
+      /* aqui, se a gente valida com rows[0] ele vai pegar o array vazio de json, então vai considerar que não é vazio pq tem um array vazio dentro!*/
+      /* por isso a validação aqui é usando rows[0][0], mas acho que no primeiro caso nao tem 2 retornos por causa do placeholder, nao sei ainda*/
+      if (rows[0][0] && !err) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    catch (e) {
+      console.log(e);
       return false;
     }
   }
