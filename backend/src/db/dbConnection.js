1e1e2e8759172a8026156d85e94a42958127ec9e
Kauê Gatto <kauegatto123@gmail.com>
2021-11-10 17:34:37 -0300

    new .env config, readme updated, npm run build no longer uses nodemon 😅

--- backend/src/db/dbConnection.js
+++ backend/src/db/dbConnection.js
@@ -1,13 +1,15 @@
 /* esse arquivo faz uma conexão com o banco de dados usando a biblioteca mysql2 e a exporta*/
 const mysql = require('mysql2');
 
+import dotenv from 'dotenv';
+
 async function main() {
     const mysql = require('mysql2/promise');
     // create the connection
     const connection = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
         database: 'prjtdah'
     });
     export { connection };
