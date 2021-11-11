02317b364d46fe1029c2187a5b823f6eb25acc89
Kauê Gatto <kauegatto123@gmail.com>
2021-11-11 11:37:38 -0300

    now checks if a environment variable for port exists

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import app from './app.js';
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const port = process.env.PORT || '3000';
+app.listen(port, () => {
+  console.log('Iniciando backend...');
+  console.log(`Escutando no endereço: http://localhost:${port}`);
 });
