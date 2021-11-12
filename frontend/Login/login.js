06e44b6602c09c40473feebb54397987fcdb1c7e
Kauê Gatto <kauegatto123@gmail.com>
2021-11-12 15:05:26 -0300

    login improved, listing all patient implemented alongside with user information being  true on config

--- frontend/Login/login.js
+++ frontend/Login/login.js
@@ -1,26 +1,33 @@
-//localStorage.clear();
+localStorage.clear();
 $(document).on("click", "#btnEntrar", function () {
-  const URL = "http://localhost:3000/tokens/login";
-  options = {
-    method: "POST",
-    headers: { 'Content-Type': 'application/json' },
-    mode: 'cors',
-    //headers: { "authentication": cookies.authentication },
-    body: JSON.stringify({ "email": $("#emailInput").val(), "password": $("#passwordInput").val() }),
-  };
+  let isResponsible = 1/*pegar pelo radiobutton*/, deuErro = 0;
+  if (isResponsible) {
+    const URL = "http://localhost:3000/tokens/login";
+    options = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      mode: 'cors',
+      body: JSON.stringify({ "email": $("#emailInput").val(), "password": $("#passwordInput").val() }),
+    };
 
-  fetch(URL, options)
-    .then(response => (response.json()))
-    .then(json => {
-      console.log(json);
-      if (json.code == 200) {
-        document.cookie = "authorization=" + json.token;
+    fetch(URL, options).then(function (response) {
+      if (!response.ok) {
+        deuErro = 1;
+      }
+      return response.json();
+    }).then(json => {
+      if (!deuErro) {
+        localStorage.setItem("authorization", json.token);
         alert("Logado com sucesso");
-        alert(document.cookie);
         window.location.href = '../responsible/html/pacientes.html';
       }
       else {
         alert(json.msg);
       }
     })
+
+  }
+  else {
+
+  }
 });
---
+++ frontend/responsible/js/pacientes.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  let deuErro = 0, responseStatus = 0;
+  const URL = "http://localhost:3000/user/findPatients";
+  if (!localStorage.getItem("authorization")) {
+    /* poupar recurso, nem faz o request*/
+    alert("Você precisa estar logado para fazer isso!");
+    window.location.href = '../../login/login.html';
+    return;
+  }
+  options = {
+    method: "GET",
+    headers: { 'Content-Type': 'application/json', 'authorization': localStorage.getItem("authorization") },
+    mode: 'cors',
+  };
+  fetch(URL, options).then(function (response) {
+    if (!response.ok) {
+      deuErro = 1;
+    }
+    responseStatus = response.status;
+    return response.json();
+  }).then(json => {
+    if (!deuErro) {
+      json.forEach(element => {
+        let patient = `<div class="infoPaciente" > <a href="relatorioPaciente.html"><span class="NomePaciente">${element.nm_paciente}</span></a> <a href="alterarDadosPaciente.html"><img src="../../img/iconEditar.png" alt="" style="margin-top: 5px; float: left;"></a> <img src="../../img/iconLixo.png" alt="" style="margin: 5px 4px; float: left;"></div>`;
+        $('.content').append(patient);
+      });
+    }
+    else {
+      alert(json.msg);
+      if (responseStatus == 498 || responseStatus == 401) {
+        window.location.href = '../../login/login.html';
+      }
+    }
+  })
+})
