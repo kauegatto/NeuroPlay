3ebeba5061bd07112983e49be1ef10fea10f6761
Douglas Reis <59988180+dougreisss@users.noreply.github.com>
2021-11-14 22:09:29 -0300

    Login paciente

--- frontend/Login/login.js
+++ frontend/Login/login.js
@@ -1,8 +1,40 @@
 localStorage.clear();
 $(document).on("click", "#btnEntrar", function () {
-  let isResponsible = 1/*pegar pelo radiobutton*/, deuErro = 0;
-  if (isResponsible) {
-    const URL = "http://localhost:3000/tokens/login";
+
+  let deuErro = 0;
+
+  if ($('#cbxCrianca:checked').val() == 1) {
+    console.log('entrou');
+    const URL = "http://localhost:3000/tokens/loginPaciente";
+
+    options = {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      mode: 'cors',
+      body: JSON.stringify({ "email": $("#emailInput").val(), "password": $("#passwordInput").val() }),
+    };
+
+    fetch(URL, options).then(function (response) {
+      if (!response.ok) {
+        deuErro = 1;
+      }
+      return response.json();
+    }).then(json => {
+      if (!deuErro) {
+        localStorage.setItem("authorization", json.token);
+        alert("Logado com sucesso");
+        window.location.href = '../kid/html/learn.html';
+      }
+      else {
+        alert(json.msg);
+      }
+    })
+
+  }
+  else if ($('#cbxAdulto:checked').val() == 0) {
+    console.log('entrou usuario');
+    const URL = "http://localhost:3000/tokens/loginUsuario";
+
     options = {
       method: "POST",
       headers: { 'Content-Type': 'application/json' },
@@ -25,9 +57,8 @@ $(document).on("click", "#btnEntrar", function () {
         alert(json.msg);
       }
     })
-
+  } else {
+    alert('Selecione alguma opção!');
   }
-  else {
 
-  }
 });
